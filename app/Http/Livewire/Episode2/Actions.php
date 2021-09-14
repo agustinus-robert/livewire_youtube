cc5e4e767ea881304e396fea7600e44be90df968
agustinus-robert <agustinus2h@gmail.com>
2021-09-14 14:28:56 +0700

    actions data masuk (Episode 2)

--- app/Http/Livewire/Episode2/Actions.php
+++ app/Http/Livewire/Episode2/Actions.php
@@ -6,6 +6,23 @@ use Livewire\Component;
 
 class Actions extends Component
 {
+    public $nama;
+    public $jurusan;
+    public $data_mahasiswa = [];
+    
+    public function method_saya(){
+      $this->data_mahasiswa = [
+        'nama' => $this->nama,
+        'jurusan' => $this->jurusan
+      ];
+      
+      return $this->data_mahasiswa;
+    }
+    
+    public function cek_jurusan(){
+        return $this->jurusan;
+    }
+    
     public function render()
     {
         return view('livewire.episode2.actions')->extends('welcome')->section('konten');
--- resources/views/livewire/episode2/actions.blade.php
+++ resources/views/livewire/episode2/actions.blade.php
@@ -1,3 +1,29 @@
 <div>
-    
+    <div class="container">
+         <div class="mb-3 text-center mt-3">
+            <label>Nama</label>
+            <input class="form-control" type="text" wire:model="nama" />
+         </div>
+        
+         <div class="mb-3 text-center mt-3">
+            <label>Jurusan</label>
+            <select wire:model="jurusan" wire:change="cek_jurusan">
+                <option value="kosong">Pilih Jurusan</option>
+                <option value="desain grafis">Desain Grafis</option>
+                <option value="sistem informasi">Sistem Informasi</option>
+            </select>
+         </div>
+        
+        <div class="col text-center">
+            <button class="btn btn-primary" wire:click="method_saya">Masuk</button>
+        </div>
+        
+        <pre>
+        <?php if(count($data_mahasiswa) > 1){ ?>
+            <?php print_r($data_mahasiswa); ?>
+        <?php } ?>
+        </pre>
+        
+        {{$jurusan}}
+    </div>
 </div>
