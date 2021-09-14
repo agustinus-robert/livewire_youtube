5337d30b71f4b46a42b961b3c4a5b79c68a1d512
agustinus-robert <agustinus2h@gmail.com>
2021-09-14 13:11:45 +0700

    wire:model dan wire:model.lazy (Episode 1)

--- app/Http/Livewire/Episode1/Wire.php
+++ app/Http/Livewire/Episode1/Wire.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class Wire extends Component
 {
+    public $nama;
+    
     public function render()
     {
         return view('livewire.episode1.wire')->extends('welcome')->section('konten');
--- resources/views/livewire/episode1/wire.blade.php
+++ resources/views/livewire/episode1/wire.blade.php
@@ -1,3 +1,10 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="container">
+       <div class="mb-3 text-center mt-3">
+        <label>Nama</label>
+        <input wire:model.lazy="nama" type="text" class="form-control">
+      </div>
+        
+        <h2>{{$nama}}</h2>
+    </div>
 </div>
