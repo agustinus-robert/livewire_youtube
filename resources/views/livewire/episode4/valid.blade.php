5d8a73dfb72cf0bcbda839d6560276a3412c0971
agustinus-robert <agustinus2h@gmail.com>
2021-09-16 07:23:16 +0700

    Validation wire:model.lazy (Episode 4)

--- resources/views/livewire/episode4/valid.blade.php
+++ resources/views/livewire/episode4/valid.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="col-md-12 mb-3 mt-2 text-center">
             <label>Nama</label>
-            <input type="text" wire:model="nama" class="form-control" />
+            <input type="text" wire:model.lazy="nama" class="form-control" />
             @error('nama') <span class="error">{{ $message }}</span> @enderror
         </div>
     </div>
